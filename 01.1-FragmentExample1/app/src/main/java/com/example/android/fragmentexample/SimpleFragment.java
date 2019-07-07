f926f0b7f49324097c3154131c3842c1e7f082be
hkucs-makerlab <makerlab@cs.hku.hk>
2019-07-07 21:44:31 +0800

    added more exercises

---
+++ 01.1-FragmentExample1/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -0,0 +1,58 @@
+package com.example.android.fragmentexample;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+
+public class SimpleFragment extends Fragment implements RadioGroup.OnCheckedChangeListener{
+    private static final int YES = 0;
+    private static final int NO = 1;
+    private RadioGroup radioGroup;
+    private View rootView;
+
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment.
+        rootView =inflater.inflate(R.layout.fragment_simple, container, false);
+        radioGroup = rootView.findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(this);
+
+        // Return the View for the fragment's UI.
+        return rootView;
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        MainActivity activity=(MainActivity)radioGroup.getContext();
+        //Log.e(this.getClass().getSimpleName(),radioGroup.getContext().getClass().toString());
+
+        View radioButton = radioGroup.findViewById(checkedId);
+        int index = radioGroup.indexOfChild(radioButton);
+        TextView textView =
+                rootView.findViewById(R.id.fragment_header);
+        switch (index) {
+            case YES: // User chose "Yes."
+                textView.setText(R.string.yes_message);
+                break;
+            case NO: // User chose "No."
+                textView.setText(R.string.no_message);
+                break;
+            default: // No choice made.
+                // Do nothing.
+                break;
+        }
+    }
+}
---
+++ 01.1-FragmentExample2/app/src/main/java/com/example/android/fragmentexample/MainActivity.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.fragmentexample;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * https://codelabs.developers.google.com/codelabs/advanced-android-training-fragments/#4
+ * demon how to use dynamic fragment
+ *  - use FragmentManager, calling getSupportFragmentManager()
+ *  - save 'isFragmentDisplayed' to instant state bundle
+ */
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    static final String STATE_FRAGMENT = "state_of_fragment";
+    private Button mButton;
+    private boolean isFragmentDisplayed = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //
+        if (savedInstanceState != null) {
+            isFragmentDisplayed =
+                    savedInstanceState.getBoolean(STATE_FRAGMENT);
+            if (isFragmentDisplayed) {
+                // If the fragment is displayed, change button to "close".
+                mButton.setText(R.string.close);
+            }
+        }
+        //
+        mButton = findViewById(R.id.open_button);
+        mButton.setOnClickListener(this);
+    }
+
+    public void displayFragment() {
+        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+        // Get the FragmentManager and start a transaction.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        // Add the SimpleFragment.
+        fragmentTransaction=fragmentTransaction.add(R.id.fragment_container,simpleFragment);
+        fragmentTransaction=fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        // Update the Button text.
+        mButton.setText(R.string.close);
+        // Set boolean flag to indicate fragment is open.
+        isFragmentDisplayed = true;
+    }
+
+    public void closeFragment() {
+        // Get the FragmentManager.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        // Check to see if the fragment is already showing.
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager
+                .findFragmentById(R.id.fragment_container);
+        if (simpleFragment != null) {
+            // Create and commit the transaction to remove the fragment.
+            FragmentTransaction fragmentTransaction =
+                    fragmentManager.beginTransaction();
+            fragmentTransaction.remove(simpleFragment).commit();
+        }
+        // Update the Button text.
+        mButton.setText(R.string.open);
+        // Set boolean flag to indicate fragment is closed.
+        isFragmentDisplayed = false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.open_button) {
+            if (!isFragmentDisplayed) {
+                displayFragment();
+            } else {
+                closeFragment();
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+}
